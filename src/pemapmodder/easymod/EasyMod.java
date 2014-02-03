60e3d0565cd0b92f6d9358edfb634da3fc565c6f
PEMapModder <>
2014-02-03 15:03:04 +0800

    TODO fields at EasyMod

--- src/pemapmodder/easymod/EasyMod.java
+++ src/pemapmodder/easymod/EasyMod.java
@@ -1,5 +1,24 @@
 package pemapmodder.easymod;
 
-public class EasyMod {
+import pemapmodder.spidermine.ServerManager;
+import android.os.Context;
+import pemapmodder.spidermine.utils.io.MyReader;
+import java.io.File;
+import android.os.Bundle;
 
+public class EasyMod {
+	public static EasyMod load(File src, Context app, int server)throws Exception{
+		ServerManager.get(server).ccl.info("Loading EasyMod from source at "+src.getAbsolutePath());
+		return new EasyMod(src, app, ServerManager.get(server));
+	}
+	private EasyMod(File f, Context app, SpiderServer server){
+		this.app=app;
+		this.server=server;
+		String src=new MyReader(f).readAll();
+		String[] resrc=src.split("<res>", 2)[1].split("\n");
+		Bundle res=new Bundle();
+		for(int i=0; i<resrc.length; i++)
+			res.putString(resrc[i].split("=>")[0], resrc[i].split("=>", 2)[1]);
+		this.res=res;
+	}
 }
--- src/pemapmodder/spidermine/android/LauncherActivity.java
+++ src/pemapmodder/spidermine/android/LauncherActivity.java
@@ -3,20 +3,38 @@ package pemapmodder.spidermine.android;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
-public class LauncherActivity extends Activity {
+public class LauncherActivity extends Activity implements View.OnClickListener{
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public final static NEW_SERVER=0x6d9753a0;
+	public final static OPEN_SERVER=0x6d9753a1;
+	@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_launcher);
+		setContentView(layout());
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public LinearLayout layout(){
+		LinearLayout ll=new LinearLayout(this);
+		TextView title=new TextView(this);
+		title.setTextSize(TypedValue.COMPLEX_UNIT_SP);
+		title.setText(R.string.ENTER_TITLE);
+		ll.addView(title);
+		Button newServer=new Button(this);
+		newServer.setOnClickListener(this);
+		newServer.setId(NEW_SERVER);
+	}
+	@Override public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.launcher, menu);
 		return true;
+	}
+	@Override public void onClick(View v){
+		switch(v.getId()){
+		case NEW_SERVER:
+			break;
+		case OPEN_SERVER:
+			break;
+		}
 	}
 
 }
