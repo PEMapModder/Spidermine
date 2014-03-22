40b577e6ac2556844f3f0e9783e7fac369b86b0f
PEMapModder <pemapmodder1970@gmail.com>
2014-03-22 13:58:30 +0800

    Added contributing notes and code style

---
+++ CONTRIBUTING.md
@@ -0,0 +1,5 @@
+Contributing Notes
+===
+If you want to contribute code to this project, please create a pull request from your fork on GitHub.
+
+Please try to keep all code in the format of CompactFormatterStyle.xml (in this repository's root directory).
--- src/pemapmodder/spidermine/network/NetworkHandler.java
+++ src/pemapmodder/spidermine/network/NetworkHandler.java
@@ -1,4 +1,4 @@
-package pemapmodder.spidermine.networking;
+package pemapmodder.spidermine.network;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -10,7 +10,9 @@ import pemapmodder.spidermine.SpiderServer;
 public class NetworkHandler extends Thread{
 	public DatagramSocket socket=null;
 	public DatagramPacket packet=null;
+	public final SpiderServer server;
 	public NetworkHandler(SpiderServer server) throws SocketException{
+		this.server=server;
 		socket=new DatagramSocket(server.getIp());
 		socket.setSoTimeout(5000);
 	}
