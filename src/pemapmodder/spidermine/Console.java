44cd05919f58cd988271d9df16f6ac961acd9341
PEMapModder <pemapmodder1970@gmail.com>
2014-03-23 21:52:21 +0800

    Finished console recode with diminished functions

--- src/pemapmodder/spidermine/Console.java
+++ src/pemapmodder/spidermine/Console.java
@@ -1,38 +1,66 @@
 package pemapmodder.spidermine;
 
-import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import android.widget.Toast;
 import pemapmodder.spidermine.objects.CommandIssuer;
+import android.util.Log;
+import android.widget.Toast;
 
-
-public class Console implements CommandIssuer{
+public class Console extends Object implements CommandIssuer{
+	public ConsoleChangedListener[] listeners={};
 	public final SpiderServer server;
-	public BufferedReader in;
-	public OutputStreamWriter out;
+	protected OutputStreamWriter out;
 	public Console(SpiderServer server){
 		this.server=server;
-		
-	}
-	public String toString(){
-		return "Console";
-	}
-	public void showLine(String line){
-		
-	}
-	@Override protected void finalize(){
+		File output=new File(server.dir, "console-log-1.log");
+		int i=2;
+		while(output.exists())
+			output=new File(server.dir, "console-log-"+Integer.toString(i++)+".log");
 		try{
-			in.close();
-			out.close();
+			out=new OutputStreamWriter(new FileOutputStream(output));
+			
 		}catch(IOException e){
-			Toast.makeText(server.app, "[ERROR] Unable to close console I/O stream: \n"+e.toString(), Toast.LENGTH_LONG).show();
+			Toast.makeText(server.app, "[ERROR] Unable to create console log file output stream.\nError type: "+e.getClass().getSimpleName()+"\nError message: "+e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+			Log.e(getClass().getName(), e.getMessage(), e);
 		}
+		addListener(new ConsoleChangedListener(){
+			@Override public void writeLine(String line){
+				try{
+					out.append(line);
+					out.write(System.getProperty("line.seperator"));
+				}catch(IOException e){
+					Toast.makeText(me().server.app, "[ERROR] Unable to write line "+line+" into console log.", Toast.LENGTH_LONG).show();
+				}
+			}
+		});
 	}
-	@Override public void out(String line){
-		showLine(line);
+	protected Console me(){
+		return this;
 	}
-	@Override public int getType(){
+	public void addListener(ConsoleChangedListener listener){
+		listeners[listeners.length]=listener;
+	}
+	public void writeLine(String line){
+		for(int i=0; i<listeners.length; i++)
+			listeners[i].writeLine(line);
+	}
+	public void runCmd(String cmdLine){
+		String[] splited=cmdLine.split(" ");
+		String[] args={};
+		for(int i=1; i<splited.length; i++){
+			args[args.length]=splited[i];
+		}
+		server.manager.cmd.invokeCmd(server.manager.cmd.get(splited[0]), args, this);
+	}
+	public static interface ConsoleChangedListener{
+		public void writeLine(String line);
+	}
+	public int getType(){
 		return TYPE_CONSOLE;
+	}
+	public void out(String line){
+		writeLine(line);
 	}
 }
